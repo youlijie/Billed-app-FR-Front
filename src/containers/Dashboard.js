1aa856b32a8ac249bc1ea6feed94f7ddd9fb0dbc
Thibault <thi95540@gmail.com>
2022-01-06 17:57:27 +0100

    [Bug Hunt] - Dashboard Done

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -144,7 +144,8 @@ export default class {
       this.counter ++
     }
 
-    bills.forEach(bill => {
+    bills.forEach(bill => { //Superposition eventListener
+      $(`#open-bill${bill.id}`).off() //Source: https://api.jquery.com/off/ Remove EventListener
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
